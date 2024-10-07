b14b9639b49bca4cda1c6c71f0f3d03a34dee994
Benceeeeeeee <144451434+Benceeeeeeee@users.noreply.github.com>
2024-10-07 21:33:34 +0200

    majdnemVege

--- main.js
+++ main.js
@@ -1,2 +1,45 @@
 import './node_modules/bootstrap/dist/css/bootstrap.css'
 import './style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hatchback = document.getElementById("hatchback");
+    const truck = document.getElementById("truck");
+    const sedan = document.getElementById("sedan");
+    const suv = document.getElementById("suv");
+    const kocsik = document.getElementById("kocsik");
+    
+    kocsik.addEventListener('change', () => {
+        const value = kocsik.value;
+
+        if (value === "osszess") {
+            hatchback.classList.remove("hidden");
+            truck.classList.remove("hidden");
+            sedan.classList.remove("hidden");
+            suv.classList.remove("hidden");
+        }
+        else if (value === "truck") {
+            hatchback.classList.add("hidden");
+            truck.classList.remove("hidden");
+            sedan.classList.add("hidden");
+            suv.classList.add("hidden");
+        }
+        else if (value === "hatchback") {
+            hatchback.classList.remove("hidden");
+            truck.classList.add("hidden");
+            sedan.classList.add("hidden");
+            suv.classList.add("hidden");
+        }
+        else if (value === "sedan") {
+            hatchback.classList.add("hidden");
+            truck.classList.add("hidden");
+            sedan.classList.remove("hidden");
+            suv.classList.add("hidden");
+        }
+        else if (value === "suv") {
+            hatchback.classList.add("hidden");
+            truck.classList.add("hidden");
+            sedan.classList.add("hidden");
+            suv.classList.remove("hidden");
+        }
+    });
+});
